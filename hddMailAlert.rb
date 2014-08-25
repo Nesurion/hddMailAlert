1971463d719193f9cbfe501ca98b3a6c27dbb81d
Nesurion <maik.schwan@gmail.com>
2014-08-25 18:53:26 +0200

    changed username and password to env variables

--- hddMailAlert.rb
+++ hddMailAlert.rb
@@ -2,8 +2,8 @@ require 'mail'
 options = { :address              => "smtp.gmail.com",
             :port                 => 587,
             :domain               => 'gmail.com',
-            :user_name            => 'username',
-            :password             => 'password',
+            :user_name            => ENV["GMAIL_USERNAME"],
+            :password             => ENV["GMAIL_PASSWORD"],
             :authentication       => 'plain',
             :enable_starttls_auto => true  }
 
