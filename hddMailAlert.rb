05ebe2348d83ec590ea98ee0727154d639eab21e
Nesurion <maik.schwan@gmail.com>
2014-08-25 22:15:10 +0200

    added config.json to set userdata.

--- hddMailAlert.rb
+++ hddMailAlert.rb
@@ -1,7 +1,11 @@
 require 'mail'
-options = { :address              => "smtp.gmail.com",
-            :port                 => 587,
-            :domain               => 'gmail.com',
+require 'json'
+
+config = JSON.parse(File.read 'config.json')
+
+options = { :address              => config["address"],
+            :port                 => config["port"],
+            :domain               => config["domain"],
             :user_name            => ENV["GMAIL_USERNAME"],
             :password             => ENV["GMAIL_PASSWORD"],
             :authentication       => 'plain',
@@ -11,21 +15,28 @@ Mail.defaults do
   delivery_method :smtp, options
 end
 
-spacePercent = `df -h /dev/sdf`.split(/\b/)[24].to_i
-spaceLeftGB = `df -h /dev/sdf`.split(/\b/)[22].to_i
+spacePercent = `df -k #{config["hddMountPoint"]}`.split(/\b/)[26].to_i
+spaceLeftGB = `df -k #{config["hddMountPoint"]}`.split(/\b/)[24].to_i / (1024 * 1024)
 
-if spacePercent >= 90 || spaceLeftGB <= 50
-Mail.deliver do
-       to 'to'
-     from 'from'
-  subject "sub"
-     body "Disk space on device is running out. #{spacePercent}% used, #{spaceLeftGB}GB left.
-On arival of the new disk:
-     		1. Add new disk to fstab
-     		2. Add new disk to hdrocker mount in rc.local
-     		3. run tvDirCloner.rb
-     		4. recreate symlinks
-     		5. add new disk to source in xbmc - and roommates xbmc"
+def sendMail(config, subjectText, bodyText)
+        Mail.deliver do
+                to config["email"]["to"]
+                from config["email"]["from"]
+                subject subjectText
+                body bodyText
+        end
+        puts "hddMailAlert sent " + Time.now.to_s
 end
-puts "hddMailAlert sent " + Time.now.to_s
+
+if spacePercent >= config["criticalPercentWarningLevel"] || spaceLeftGB <= config["criticalGBWarningLevel"]
+	sendMail(config, 
+	"Disk is full!", 
+	"Disk is full! #{spacePercent}% used, #{spaceLeftGB}GB left.")
+else
+	if spacePercent >= config["percentWarningLevel"] || spaceLeftGB <= config["GBWarningLevel"]
+		sendMail(config,
+		"Disk space running out",
+		"Disk space on device is running out. #{spacePercent}% used, #{spaceLeftGB}GB left.")
+	end
 end
+
